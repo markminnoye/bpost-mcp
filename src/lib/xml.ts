e28919ec42f3257ce728ac0542992105fb9a9c22
Mark Minnoye <mark@minnoye.com>
2026-03-31 15:04:33 +0200

    feat: add fast-xml-parser singleton (buildXml/parseXml)

---
+++ src/lib/xml.ts
@@ -0,0 +1,25 @@
+import { XMLBuilder, XMLParser } from 'fast-xml-parser'
+
+const PARSER_OPTIONS = {
+  ignoreAttributes: false,
+  attributeNamePrefix: '@_',
+  allowBooleanAttributes: true,
+  parseAttributeValue: false, // keep strings as-is per BPost spec
+}
+
+const BUILDER_OPTIONS = {
+  ignoreAttributes: false,
+  attributeNamePrefix: '@_',
+  format: false,
+}
+
+export const xmlParser = new XMLParser(PARSER_OPTIONS)
+export const xmlBuilder = new XMLBuilder(BUILDER_OPTIONS)
+
+export function parseXml<T>(xml: string): T {
+  return xmlParser.parse(xml) as T
+}
+
+export function buildXml(obj: Record<string, unknown>): string {
+  return `<?xml version="1.0" encoding="ISO-8859-1"?>\n${xmlBuilder.build(obj)}`
+}
